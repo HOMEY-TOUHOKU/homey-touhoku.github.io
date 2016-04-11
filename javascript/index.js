b43d1c11bb7915d8d7c161296080b4cceabac2e3
TaikiFnit <taikifnitspg@gmail.com>
2016-04-11 16:19:39 +0900

    impmelemt news index on index.html

--- javascript/index.js
+++ javascript/index.js
@@ -3,9 +3,17 @@ $(function() {
             type: "GET",
             url: "http://homey.php.xdomain.jp/news/*",
             dataType: "json",
-            success: function(msg) {
+            success: function(data) {
                 console.log('success');
-                console.log(msg);
+                console.log(data);
+
+                var code = '';
+
+                for(var i = 0; i < data.length; i++) {
+                    code += '<li><a href="/news/' + data[i]['id'] + '">' + data[i]['created'] + ' ' + data[i]['content'] + '</a></li>';
+                }
+
+                $('.newsList').html(code);
             },
             error: function(e) {
                 console.log(e);
