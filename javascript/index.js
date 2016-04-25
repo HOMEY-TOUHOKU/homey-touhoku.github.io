5ba7d8604d411b64f6f12e15cb84599ce9b19236
TaikiFnit <taikifnitspg@gmail.com>
2016-04-25 21:00:12 +0900

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -21,7 +21,7 @@ $(function() {
         });
    $.ajax({
             type: "GET",
-            url: "http://homey.php.xdomain.jp/news_image/3-1.png",
+            url: "http://homey.php.xdomain.jp/news_image/3-1.json",
             success: function(data) {
                 console.log('success');
                 console.log(data);
