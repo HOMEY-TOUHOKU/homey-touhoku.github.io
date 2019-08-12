208fa56500296124f9e6fe2496bbc44b59781817
TaikiFnit <taikifnitspg@gmail.com>
2019-08-13 07:02:38 +0900

    news表示のアンカーリンクに対応

--- javascript/news.js
+++ javascript/news.js
@@ -12,8 +12,14 @@ $(function() {
                 const date = `${created.getFullYear()}-${created.getMonth() + 1}-${created.getDate()}`;
                 const date_jp = `${created.getFullYear()}年${created.getMonth() + 1}月${created.getDate()}日`;
 
-                return `<li><section><h3><a href="${item.link}">${item.title}</a></h3><p><time datetime="${date}">${date_jp}</time></p><div class="content">${item.description}</div></li></section>`;
+                return `<li><section id="${item.title}"><h3><a href="${item.link}">${item.title}</a></h3><p><time datetime="${date}">${date_jp}</time></p><div class="content">${item.description}</div></li></section>`;
             }).join(''));
+
+            console.log(decodeURI(location.hash));
+
+            $('html,body').animate({
+                scrollTop: $(decodeURI(location.hash)).offset().top - 51 - 40,
+            }, 1000);
         },
         error: function(e) {
             console.log(e);
