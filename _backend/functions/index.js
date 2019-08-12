16e67083280662457a8b57e21500a3a2fbf3cc5b
TaikiFnit <taikifnitspg@gmail.com>
2019-08-13 05:29:15 +0900

    CORS許可

--- _backend/functions/index.js
+++ _backend/functions/index.js
@@ -1,17 +1,20 @@
 const functions = require('firebase-functions');
 const Feed = require('rss-to-json');
+const cors = require('cors')({origin: true});
 
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.blog = functions.https.onRequest((request, response) => {
- Feed.load('http://rssblog.ameba.jp/tokotokoami/rss20.xml', (err, rss) => {
-  if (err) {
-   response.sendStatus(500);
-   return;
-  }
+ cors(request, response, () => {
+  Feed.load('http://rssblog.ameba.jp/tokotokoami/rss20.xml', (err, rss) => {
+   if (err) {
+    response.sendStatus(500);
+    return;
+   }
 
-  response.send(rss);
+   response.send(rss);
+  });
  });
 });
