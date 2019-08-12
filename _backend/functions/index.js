d9a798f7c1f1aa0f3e21f0b7aa9ec701d250a332
TaikiFnit <taikifnitspg@gmail.com>
2019-08-13 05:24:16 +0900

    rss-to-json package をつかって再実装

--- _backend/functions/index.js
+++ _backend/functions/index.js
@@ -1,8 +1,17 @@
 const functions = require('firebase-functions');
+const Feed = require('rss-to-json');
+
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.blog = functions.https.onRequest((request, response) => {
+ Feed.load('http://rssblog.ameba.jp/tokotokoami/rss20.xml', (err, rss) => {
+  if (err) {
+   response.sendStatus(500);
+   return;
+  }
+
+  response.send(rss);
+ });
+});
