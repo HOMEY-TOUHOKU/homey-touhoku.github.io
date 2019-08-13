20d738a726010355ee15ddecd7688f373102f19a
TaikiFnit <taikifnitspg@gmail.com>
2019-08-14 05:13:20 +0900

    メールCGI実装完了

--- _backend/functions/index.js
+++ _backend/functions/index.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions');
 const Feed = require('rss-to-json');
 const cors = require('cors')({origin: true});
+const nodemailer = require("nodemailer");
 
 
 // // Create and Deploy Your First Cloud Functions
@@ -17,4 +18,31 @@ exports.blog = functions.https.onRequest((request, response) => {
    response.send(rss);
   });
  });
+});
+
+exports.send_mail = functions.https.onRequest(async (request, response) => {
+ if (request.method !== 'POST') {
+  response.status(405).send('Method Not Allowed');
+  return;
+ }
+
+ let transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // true for 465, false for other ports
+  auth: {
+   user: functions.config().api.mail_user, // generated ethereal user
+   pass: functions.config().api.mail_pass // generated ethereal password
+  }
+ });
+
+ // send mail with defined transport object
+ let info = await transporter.sendMail({
+  from: functions.config().api.mail_user, // sender address
+  to: functions.config().api.mail_user, // list of receivers
+  subject: "HOMEY お問い合わせフォームより", // Subject line
+  text: `${request.body.message}\n\n---\n\n送信者: ${request.body.name}\n送信者のメールアドレス: ${request.body.email}` // plain text body
+ });
+
+ response.send({status: true});
 });
